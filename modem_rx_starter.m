d9b23cfacb02c372f7b0f634c9d9f8a383f4e546
Thomas <thomas.jagielski@yahoo.com>
2021-05-04 17:37:29 -0400

    Add updates

--- modem_rx_starter.m
+++ modem_rx_starter.m
@@ -12,14 +12,39 @@ start_idx = find_start_of_signal(y_r,x_sync);
 y_t = y_r(start_idx+length(x_sync):end); % y_t is the signal which starts at the beginning of the transmission
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%  Put your decoder code here
-%%
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Put your decoder code here
+% create a cosine with analog frequency f_c
+c = cos(2*pi*f_c/Fs*[0:length(y_t)-1]');
+% create the transmitted signal
+y_rx = y_t.*c;
+%plot(y_rx)  % visualize the transmitted signal
 
+% Low Pass Filter
+W = 2*pi*1;
+%t = [-(length(y_t)/2):1:((length(y_t)/2)-1)]*(1/Fs);
+t = [-10:1:10]*(1/Fs);
+h = W/pi*sinc(W/pi*t);
+
+x_d = conv(y_rx, h);
+plot(x_d(1:5000))
+
+
+
+%%
+x_d(1) = 0;
+for l=2:length(x_d)
+    if (x_d(l) > 0.5)
+        x_d(l) = 1;
+    elseif (x_d(l) < 0.5)
+        x_d(l) = 0;
+    else
+        x_d(l) = x_d(l-1);
+    end
+end
+
+plot(x_d(1:5000))
 
 % convert to a string assuming that x_d is a vector of 1s and 0s
 % representing the decoded bits
-BitsToString(x_d)
+BitsToString(x_d(1:5004))
 
